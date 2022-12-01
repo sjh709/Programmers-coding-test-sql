a80b703770df7640844340a263045fee842d8da2
Jihee <42454759+sjh709@users.noreply.github.com>
2022-12-02 03:44:37 +0900

    Create 19.sql

---
+++ Level-1/19.sql
@@ -0,0 +1,7 @@
+select a.flavor
+  from first_half a,
+       icecream_info b
+ where a.flavor = b.flavor
+   and a.total_order >= 3000
+   and b.ingredient_type = 'fruit_based'
+ order by a.total_order desc;
