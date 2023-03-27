09fc3690f9a1d98a7b13fcf1d39d0c04c542a7bb
Jihee <42454759+sjh709@users.noreply.github.com>
2023-03-27 20:19:31 +0900

    Create 22.sql

---
+++ Level-1/22.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT history_id, car_id,
+       to_char(start_date, 'yyyy-mm-dd') as start_date,
+       to_char(end_date, 'yyyy-mm-dd') as end_date,
+       case when end_date - start_date + 1 >= 30 then '장기 대여'
+            else '단기 대여' end as rent_type
+  from car_rental_company_rental_history
+ where to_char(start_date, 'yyyymm') = '202209'
+ order by history_id desc;
