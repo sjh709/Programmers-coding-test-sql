5c9adb8ff3e031010c7db46ec0098289ca0d5e8d
Jihee <42454759+sjh709@users.noreply.github.com>
2023-04-02 03:23:55 +0900

    Create 5.sql

---
+++ Level-3/5.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+select *
+  from places
+ where host_id in (select host_id
+                     from places
+                    group by host_id
+                   having count(*) > 1)
+ order by id;
